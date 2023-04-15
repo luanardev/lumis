8260de6c80a2800c9a316e33d70f73118bec7f4b
Brian Simpokolwe <90185393+luanardev@users.noreply.github.com>
2023-04-15 18:43:49 +0200

    Update EventServiceProvider.php

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -5,6 +5,8 @@ namespace App\Providers;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
+use SocialiteProviders\LaravelPassport\LaravelPassportExtendSocialite;
+use SocialiteProviders\Manager\SocialiteWasCalled;
 
 class EventServiceProvider extends ServiceProvider
 {
@@ -16,6 +18,10 @@ class EventServiceProvider extends ServiceProvider
     protected $listen = [
         Registered::class => [
             SendEmailVerificationNotification::class,
+        ],
+
+        SocialiteWasCalled::class => [
+            LaravelPassportExtendSocialite::class.'@handle',
         ],
     ];
 
