022889b2c94f1fdf4e67c74052d32a818ab24c54
Brian Simpokolwe <90185393+luanardev@users.noreply.github.com>
2023-04-15 18:44:04 +0200

    Update services.php

--- config/services.php
+++ config/services.php
@@ -31,4 +31,11 @@ return [
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
 
+    'laravelpassport' => [
+        'client_id' => env('OAUTH_CLIENT_ID'),
+        'client_secret' => env('OAUTH_CLIENT_SECRET'),
+        'redirect' => env('OAUTH_CALLBACK'),
+        'host' => env('OAUTH_HOST'),
+    ],
+
 ];
